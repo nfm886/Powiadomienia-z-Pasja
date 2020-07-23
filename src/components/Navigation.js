937e46c210e36b3d1b28de938567b1056c050d1b
nfm886 <piotrekpoczta007@gmail.com>
2020-07-23 09:15:54 +0200

    Top navigation bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,70 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import SettingsIcon from '@material-ui/icons/Settings';
+import LoyaltyIcon from '@material-ui/icons/Loyalty';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+
+const useStyles = makeStyles({
+  root: {
+    width: 282,
+    backgroundColor: "#0984e3"
+  },
+  navLink: {
+    color: "#ffffff",
+    '&$selected': {
+      color: "#ffffff"
+    },
+  },
+  selected: {}
+});
 
 const Navigation = () => {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(2);
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/settings">Ustawienia</Link>
-        </li>
-        <li>
-          <Link to="/watchlist">Obserwowane</Link>
-        </li>
-        <li>
-          <Link to="/">Powiadomienia</Link>
-        </li>
-      </ul>
-    </nav>
+    <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      showLabels
+      className={classes.root}
+    >
+      <BottomNavigationAction
+        label="Ustawienia"
+        icon={<SettingsIcon />}
+        component={Link}
+        to="/settings"
+        classes={{
+          root: classes.navLink,
+          selected: classes.selected
+        }}
+      />
+      <BottomNavigationAction
+        label="Obserwowane"
+        icon={<LoyaltyIcon />}
+        component={Link}
+        to="/watchlist"
+        classes={{
+          root: classes.navLink,
+          selected: classes.selected
+        }}
+      />
+      <BottomNavigationAction
+        label="Powiadomienia"
+        icon={<NotificationsIcon />}
+        component={Link}
+        to="/"
+        classes={{
+          root: classes.navLink,
+          selected: classes.selected
+        }}
+      />
+    </BottomNavigation>
   );
 }
 
