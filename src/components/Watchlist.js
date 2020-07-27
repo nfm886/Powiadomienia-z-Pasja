62bc1f72faaa0eb27321957a054f9c856779e038
nfm886 <piotrekpoczta007@gmail.com>
2020-07-27 11:08:35 +0200

    Working watchlist

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -1,7 +1,107 @@
-import React from 'react';
+/* global chrome */
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import cheerio from 'cheerio';
+import { html_unescape, parseWhen, parseTime } from '../helpers/functions';
+import { Box, CircularProgress, makeStyles } from '@material-ui/core';
+import LoyaltyIcon from '@material-ui/icons/Loyalty';
+
+const useStyles = makeStyles((theme) => ({
+  circularProgress: {
+    position: "fixed",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)"
+  }
+}));
+
+const followList = [];
+const watchList = [];
+const lastQuestions = [];
+
+chrome.storage.sync.get(['followed'], storage => {
+  if (storage.followed !== undefined && storage.followed.length > 0) {
+    for (let i = 0; i < storage.followed.length; i++) {
+      followList.push({
+        name: storage.followed[i].name,
+        url: storage.followed[i].url
+      });
+    }
+  }
+});
+
+const GettingPosts = (object, iteration) => new Promise((resolve, reject) => {
+  if (iteration >= 0) {
+    axios.get(object[iteration].url).then((response) => {
+
+      const html = response.data;
+      const $ = cheerio.load(html);
+
+      const title = $(html).find('div.qa-q-item-main > div.qa-q-item-title > a > span');
+      const when = $(html).find('div.qa-q-item-main > span > span > span.qa-q-item-when > span.qa-q-item-when-data');
+      const urls = $(html).find('div.qa-q-item-main > div.qa-q-item-title > a');
+
+      lastQuestions.push({
+        tag: object[iteration].name,
+        title: title[0].children[0].data.trunc(65).toString(),
+        url: urls[0].attribs.href.replace('../', 'https://forum.pasja-informatyki.pl/')
+      });
+
+      title.map((key, item) => {
+        return watchList.push({
+          title: html_unescape(title[key].children[0].data).trunc(65).toString(),
+          when: parseWhen(when[key].children[0].data),
+          date: parseTime(when[key].children[0].data),
+          tag: object[iteration].name,
+          url: urls[key].attribs.href.replace('../', 'https://forum.pasja-informatyki.pl/')
+        });
+      });
+
+      iteration = iteration - 1;
+      resolve(GettingPosts(object, iteration));
+    });
+  } else {
+    console.log(watchList)
+    resolve(watchList);
+  }
+})
 
 const Watchlist = () => {
-  return <h1>Obserwowane</h1>
+
+  const classes = useStyles();
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    GettingPosts(followList, followList.length - 1).then((data) => {
+      setData(data.sort((a, b) => b.date - a.date));
+      chrome.storage.local.set({ lastQuestions: lastQuestions });
+    });
+  }, []);
+
+  return (
+    <Box id='#nfyContainerInbox'>
+      {
+        (data === null) ?
+          <Box className={classes.circularProgress}>
+            <CircularProgress color="primary" />
+          </Box> :
+          data.map((item) => {
+            return (
+              <Box class="itemBox">
+                <Box class="nfyItemLine">
+                  <p class="nfyWhat">
+                    <span class="tag-icon"><LoyaltyIcon /></span><a href={item.url} target="_blank" rel="noopener noreferrer">{item.title}</a>
+                  </p>
+                  <p class="nfyTime">{item.when} w <span class="show-tag">{item.tag}</span></p>
+                </Box>
+              </Box>
+            )
+          })
+      }
+    </Box>
+  )
 }
 
 export default Watchlist;
