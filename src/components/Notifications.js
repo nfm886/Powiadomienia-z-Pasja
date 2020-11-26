97cc73fae2c856abbed8fe12bcf2aa6be8cc363b
nfm886 <piotrekpoczta007@gmail.com>
2020-11-26 19:49:15 +0100

    Redesigned notifications list

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -2,25 +2,34 @@
 
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import cheerio from 'cheerio';
 import { makeStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import "../index.css";
-import { Paper } from '@material-ui/core';
+import { Paper, Box, Container, List, ListItem, ListItemText, ListItemAvatar, Avatar } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    '& > * + *': {
-      marginLeft: theme.spacing(2),
-    },
-    position: "fixed",
+    height: "467px",
+    width: "360px",
+    padding: "0px"
+  },
+  progress: {
+    position: "absolute",
     top: "50%",
     left: "50%",
-    transform: "translate(-50%, -50%)"
+    transform: "translate(-50%,-50%)"
+  },
+  listItem: {
+    "&:hover": {
+      backgroundColor: "#dfe6e9",
+      cursor: "pointer"
+    }
   }
 }));
 
-chrome.browserAction.setBadgeText({text: ''})
+chrome.browserAction.setBadgeText({text: ''});
+
+const nfyWhat = [];
 
 const FormData = require('form-data');
 const form = new FormData();
@@ -33,30 +42,64 @@ const Notifications = () => {
   useEffect(() => {
     axios.post('https://forum.pasja-informatyki.pl/eventnotify', form).then(
       (response) => {
-        const data = response.data;
-        const a_tag = /<a (.*)>/g;
-        setNotifications(data.replace(a_tag, '<a target="_blank" $1>'));
+
+        const html = response.data
+        const $ = cheerio.load(html);
+
+        const nfyWhatClass = $(html).find('.nfyWhat');
+        
+        let i;
+        
+        for(i = 0; i < nfyWhatClass.length; i++) {
+          nfyWhat.push({
+            action: nfyWhatClass[i].children[0].data.trim(),
+            title: nfyWhatClass[i].parent.children[1].children[1].children[0].data.trim(),
+            url: nfyWhatClass[i].children[1].attribs.href,
+            when: nfyWhatClass[i].parent.children[4].prev.children[0].data.trim()
+          })
+        }
+
+        setNotifications(nfyWhat);
       }
     )
-  });
+  }, []);
+
+  const handleClick = (url) => {
+    window.open(url)
+  }
 
   if(notifications === 'Userid is empty!') {
     return (
-      <Paper style={{height: '467px'}}>
-        <div className={classes.root}>
+      <Paper className={classes.root}>
           <h3>Nie jesteś zalogowany.</h3>
-        </div>
       </Paper>
     )
   } else {
     return (
-      <Paper style={{height: '467px'}}>
-        <div className={classes.root}>
+      <Container className={classes.root}>
           {
-            (notifications === null) ? <CircularProgress color="primary" /> : <div dangerouslySetInnerHTML={{ __html: notifications }} class="notifications" />
+          (notifications === null) ? 
+            <Box className={classes.progress}>
+              <CircularProgress color="primary" />
+            </Box> :
+              <Paper elevation={0}>
+                <List className={classes.root}>
+                {notifications.map((item, index) => {
+                  return (
+                      <ListItem onClick={ () => handleClick(item.url) } className={classes.listItem}>
+                        <ListItemAvatar>
+                          <Avatar>
+                            <h3>{item.action.slice(0,1)}</h3>
+                          </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText primary={item.title} secondary={item.when} />
+                      </ListItem>
+                  )
+                })}
+                </List>
+              </Paper>
           }
-        </div>
-      </Paper>
+      </Container>
     );
   }
 }
